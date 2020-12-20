0652745107e0b5a08c8f6966e5c41288be94c74e
almohelo <almoyner.heloise@gmail.com>
2020-12-20 23:36:35 +0100

    mise en place des infos en JS

--- js/page_confirmation.js
+++ js/page_confirmation.js
@@ -1,15 +1,33 @@
 let order = JSON.parse(localStorage.getItem("order"));
-let i = 0;
 
+//recherche des éléments dans la page HTML
+let mySection = document.getElementById("confirm");
 let nomPrenom = document.getElementById("contact");
 let idCommandeHTML = document.getElementById("nCmd");
 let totalCommande = document.getElementById("totalCmd");
 
-console.log(order.products)
-console.log(order.contact)
-console.log(order.orderId)
+//récupération de la somme total des articles
+let sommeTotal = 0;
+let tableau = order.products;
+tableau.forEach(element => sommeTotal += element.price)
 
-let idCommande = order.orderId;
+//insertion des éléments dans la page HTML
+insertName = (div, myName) => {
+    let myP = document.createElement("p");
+    myP.innerHTML = myName;
+    mySection.appendChild(myP)
+}
+insertLine = (div, myId) => {
+    let mySecondP = document.createElement("p");
+    mySecondP.innerHTML = myId;
+    mySection.appendChild(mySecondP)
+}
+const insertLink = (div, _id) => {
+    let link = document.createElement("a");
+    link.innerHTML = "<a href=page_accueil.html>Retour à la page d'accueil</a>";
+    div.appendChild(link);
+}
 
-
-insertID 
+insertName(mySection, "Merci " + order.contact.firstName + " " + order.contact.lastName + " pour votre commande !");
+insertLine(mySection, "Nous vous confirmons sa réception au n° " + order.orderId + " d'un prix total de " + sommeTotal / 100 + " €");
+insertLink(mySection);
