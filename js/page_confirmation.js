e35b1ceef7146b17b34a3ce17b2d66f9e5597d86
almohelo <almoyner.heloise@gmail.com>
2020-12-21 00:07:55 +0100

    mise en place du css et en version responsive

--- js/page_confirmation.js
+++ js/page_confirmation.js
@@ -12,22 +12,25 @@ let tableau = order.products;
 tableau.forEach(element => sommeTotal += element.price)
 
 //insertion des éléments dans la page HTML
+let info = document.createElement("div");
+info.className = "information";
+
 insertName = (div, myName) => {
     let myP = document.createElement("p");
     myP.innerHTML = myName;
-    mySection.appendChild(myP)
+    info.appendChild(myP)
 }
 insertLine = (div, myId) => {
     let mySecondP = document.createElement("p");
     mySecondP.innerHTML = myId;
-    mySection.appendChild(mySecondP)
+    info.appendChild(mySecondP)
 }
 const insertLink = (div, _id) => {
     let link = document.createElement("a");
     link.innerHTML = "<a href=page_accueil.html>Retour à la page d'accueil</a>";
     div.appendChild(link);
 }
-
-insertName(mySection, "Merci " + order.contact.firstName + " " + order.contact.lastName + " pour votre commande !");
-insertLine(mySection, "Nous vous confirmons sa réception au n° " + order.orderId + " d'un prix total de " + sommeTotal / 100 + " €");
+mySection.appendChild(info)
+insertName(info, "Merci " + order.contact.firstName + " " + order.contact.lastName + " pour votre commande !");
+insertLine(info, "Nous vous confirmons sa réception au n° " + order.orderId + " d'un prix total de " + sommeTotal / 100 + " €");
 insertLink(mySection);
